fd2db4e9e712cb1d97c522c4b25b15c1a3a44202
Md.Abdullah Al Maruf <thefrankmaruf@gmail.com>
2022-03-20 00:17:22 +0600

    107

---
+++ admin/admin/src/secure/dashboard/Dashboard.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Wrapper from '../Wrapper'
+
+const Dashboard = () => {
+    return (
+        <React.Fragment>
+      <Wrapper>
+      Dashboard
+      </Wrapper>
+        </React.Fragment>
+    )
+}
+
+export default Dashboard
